92968626f56aa0b7f3ddb0043346291b70070df8
Sudip1014 <mahtabhossain1014@gmail.com>
2023-11-06 16:23:02 +0600

    posted jobs

--- src/Pages/MyPostedJobs/MyPostedJobs.jsx
+++ src/Pages/MyPostedJobs/MyPostedJobs.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import useAxios from "../../Hooks/useAxios";
+import useAuth from "../../Hooks/useAuth";
+import PostedJobsCard from "./PostedJobsCard";
 
 const MyPostedJobs = () => {
-    return (
-        <div>
-            
+  const { user } = useAuth();
+  const [postData, setPostData] = useState([]);
+  const exios = useAxios();
+
+
+  useEffect(() => {
+    exios.get(`/job-posts?email=${user?.email}`).then((data) => {
+      setPostData(data.data);
+    });
+  }, [user, exios]);
+
+  console.log(postData)
+
+  return (
+    <>
+      <section>
+        <div className="section-title mt-5">
+          <h2>My Posted Jobs</h2>
         </div>
-    );
+
+        <div className="mt-3 flex justify-center gap-5">
+          {postData?.map((post) => (
+            <PostedJobsCard key={post._id} post={post} />
+          ))}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default MyPostedJobs;
